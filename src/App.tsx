473cccd6ab2856b4be5a292914bd28f90806fe5e
Ramachandra Anirudh Vemulapalli <anirudh@Ramachandras-MacBook-Air.local>
2024-04-10 19:45:02 +0530

    About page is added to app page

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,13 @@ import {
 } from "react-router-dom";
 import RootLayout from "./layouts/RootLayout";
 import Homepage from "./pages/Homepage";
+import About from "./pages/About";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route element={<RootLayout />}>
       <Route index element={<Homepage />} />
+      <Route path="about" element={<About />} />
     </Route>
   )
 );
