5ed99f36465f4c5615d7c792495f9137cec02fa5
Ramachandra Anirudh Vemulapalli <anirudh@Ramachandras-MacBook-Air.local>
2024-04-10 20:08:42 +0530

    Location page is added to App page

--- src/App.tsx
+++ src/App.tsx
@@ -9,12 +9,14 @@ import RootLayout from "./layouts/RootLayout";
 import Homepage from "./pages/Homepage";
 import About from "./pages/About";
 import Careers from "./pages/Careers";
+import Locations from "./pages/Locations";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route element={<RootLayout />}>
       <Route index element={<Homepage />} />
       <Route path="about" element={<About />} />
+      <Route path="locations" element={<Locations />} />
       <Route path="careers" element={<Careers />} />
       <Route path="*" element={<Navigate to="/" />} />
     </Route>
