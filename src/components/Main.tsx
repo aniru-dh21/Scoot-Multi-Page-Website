683e7427e81cf84e4893e657a45da7b1f66536d1
Ramachandra Anirudh Vemulapalli <anirudh@Ramachandras-MacBook-Air.local>
2024-04-10 17:27:42 +0530

    Main Component is designed

---
+++ src/components/Main.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Main({ children }: React.PropsWithChildren) {
+  return (
+    <main className="mt-[4.5rem] mb-[7.5rem] flex flex-col gap-[7.5rem] md:mt-36 xl:mt-[7.5rem] xl:mb-40">
+      {children}
+    </main>
+  );
+}
